d33ab6e74e4d1ee410e42f80353a6f0db3bbc636
Posterizedsoul <beebekisme@gmail.comm>
2022-10-31 21:38:34 +0545

    Added: Error Handler in ML model and better Test Cases

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import requests
 
 url = 'http://127.0.0.1:8000/recommend'
 input_data = {
-    "place_name": "Phewa Lake"
+    "place_name": "Phewa Lake",
+    "place_name": None,
+    "place_name":123224
 }
 
 input_json = json.dumps(input_data)
